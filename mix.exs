6b491881ab5bf4620612c4484d3d29146f2c26c1
Ned Rihine <ned.rihine@gmail.com>
2024-06-09 20:22:51 +0900

    Bump version to elixir 1.10

--- mix.exs
+++ mix.exs
@@ -6,18 +6,29 @@ defmodule Calliope.Mixfile do
   def project do
     [ app: :calliope,
       version: "0.4.2",
-      elixir: "~> 1.4",
-      deps: [{:ex_doc, ">= 0.0.0", only: :dev}],
-      package: [
-        files: ["lib", "mix.exs", "README*", "LICENSE*"],
-        contributors: ["Johnny Winn", "Stephen Pallen"],
-        maintainers: ["Johnny Winn", "Stephen Pallen"],
-        licenses: ["Apache 2.0"],
-        links: %{ "Github" => "https://github.com/nurugger07/calliope" }
-      ],
+      elixir: "~> 1.10",
+      deps: deps(),
+      package: package(),
       description: """
       An Elixir library for parsing haml templates.
       """
     ]
   end
+
+  def deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      contributors: ["Johnny Winn", "Stephen Pallen"],
+      maintainers: ["Johnny Winn", "Stephen Pallen"],
+      licenses: ["Apache 2.0"],
+      links: %{ "Github" => "https://github.com/nurugger07/calliope" }
+    ]
+  end
+  
 end
